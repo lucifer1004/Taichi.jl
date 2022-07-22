b36f169b28d6a3971af3786af81904f30d94c68a
Documenter.jl <documenter@juliadocs.github.io>
2022-07-22 15:35:23 +0000

    build based on 5b67d5f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Taichi","category":"page"},{"location":"#Taichi","page":"Home","title":"Taichi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Taichi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Taichi]","category":"page"}]
+}
